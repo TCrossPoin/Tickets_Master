ee4a5aacd3f1b06dc09294e563084fd7262e472a
TestingCPT <112604798+TCrossPoin@users.noreply.github.com>
2025-07-19 10:01:04 +0530

    app.py

--- app.py
+++ app.py
@@ -675,4 +675,6 @@ scheduler.add_job(check_unacknowledged_tickets, 'interval', hours=1)
 scheduler.start()
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
